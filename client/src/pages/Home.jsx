233c2b7533193b5c00639bf09f68875480b1f681
jagonzalez <jagonzalez@MSI.>
2024-10-26 11:46:16 -0700

    added a home page feature

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+// client/src/pages/Home.jsx
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="flex flex-col items-center p-6">
+            <h1 className="text-4xl font-bold text-center mt-4">Welcome to IronPulse Fitness Website</h1>
+            <p className="mt-2 text-center text-lg text-gray-600">
+                Explore our fitness programs, Compete with friends, track your progress, and stay healthy!
+            </p>
+        </div>
+    );
+};
+
+export default Home;
